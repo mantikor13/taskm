448c73c3413022392c4c4af920c7e4d6a3d734b8
mantikor13 <roman.web.kuznetsov@gmail.com>
2018-04-10 17:48:32 +0300

    basic config

---
+++ src/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app">
+        <h1 class="title">{{message}}</h1>
+        <div class="content">lorem*2</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                message: 'Hellow, world'
+            }
+        }
+    }
+</script>
+
+<style lang="stylus">
+    .app{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        .content{
+            font-size: 30px;
+        }
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+
+};
+module.exports = conf;
